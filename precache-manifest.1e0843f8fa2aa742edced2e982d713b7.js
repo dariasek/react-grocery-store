56af77eaad58af087db51432cc3419ca8081c469
Daria <fordariasek@gmail.com>
2020-07-24 17:17:58 +0300

    Updates

---
+++ precache-manifest.1e0843f8fa2aa742edced2e982d713b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f094ff03c863a2fbec125a90e9d6d93d",
+    "url": "/react-grocery-store/index.html"
+  },
+  {
+    "revision": "112d77f40ad0473373e1",
+    "url": "/react-grocery-store/static/css/main.9f0bb50d.chunk.css"
+  },
+  {
+    "revision": "faac507fefdd92d4e541",
+    "url": "/react-grocery-store/static/js/2.921f7565.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-grocery-store/static/js/2.921f7565.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "112d77f40ad0473373e1",
+    "url": "/react-grocery-store/static/js/main.be9595e7.chunk.js"
+  },
+  {
+    "revision": "6715581a506562caf3c7",
+    "url": "/react-grocery-store/static/js/runtime-main.87968a56.js"
+  }
+]);
